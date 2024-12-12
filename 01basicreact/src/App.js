e10e9dec60ad469766799dab46a3afbc99341490
ishij19 <142041733+ishij19@users.noreply.github.com>
2024-12-12 13:01:11 +0000

    01

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+   <h1>Chai aur javascript| IJ</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+
+
+  return (
+    <h1>chai aur react with vite app | IJ</h1>
+  )
+}
+
+export default App
