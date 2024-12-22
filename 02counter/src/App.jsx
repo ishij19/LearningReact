1353bf53859b31d4d5a59b82a538e7b65a118061
ishi <ishicjain19@gmail.com>
2024-12-22 14:31:07 +0530

    folder changed

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [Counter,setCounter]=  useState(15)
+
+let Counter=15
+const addValue=()=>{
+  console.log("clicked",Counter);
+  Counter=Counter+1
+  
+}
+
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>Counter value: {Counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {Counter}</button>
+      <br />
+      <button>remove value {Counter}</button>
+      <p>footer: {Counter}</p>
+
+    </>
+  )
+}
+
+export default App
